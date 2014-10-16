3fc6a7b900711db69a9139b89efa0a0b577b8010
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2014-10-17 00:08:22 +0200

    Programación de Scripts con Dummie

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float fuerzaSalto = 200f;
+	private bool grounded = true;
+	public LayerMask ground;
+	public Transform OPA, OPB;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Input.GetMouseButton(0) && grounded){
+			rigidbody2D.AddForce(new Vector2(0,fuerzaSalto));
+		}
+	}
+
+	void FixedUpdate (){
+		grounded = Physics2D.OverlapArea (OPA.position, OPB.position, ground);
+	}
+
+}
