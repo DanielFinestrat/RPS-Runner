ed1aec2f0f96a71ec8f97190db7a7043bc7ace60
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2014-10-23 02:04:30 +0200

    Añadido el Slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,36 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float fuerzaSalto = 200f;
-	private bool grounded = true;
+
 	public LayerMask ground;
 	public Transform OPA, OPB;
 
-	void Update () {
-		if(Input.GetMouseButton(0) && grounded){
-			rigidbody2D.AddForce(new Vector2(0,fuerzaSalto));
-		}
+	private bool grounded = true;
+	private bool slideing = false;
+
+	private Animator animator;
+
+	void Awake(){
+		animator = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate (){
 		grounded = Physics2D.OverlapArea (OPA.position, OPB.position, ground);
+	}
+
+	void Update () {
+		if(Input.GetKeyDown("up") && grounded && !slideing){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,fuerzaSalto);
+		}
+		if(Input.GetKeyDown("down") && grounded){
+			slideing = true;
+			animator.SetBool("slideing", slideing);
+		}
+	}
+
+	void stopSlideing(){
+		slideing = false;
+		animator.SetBool ("slideing", slideing);
 	}
 
 }
