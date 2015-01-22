e1d7c115c5cbdc7f45675fb096e6dbb648cfc905
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2015-01-22 16:42:16 +0100

    Gitignore actualizado, repositorio limpio

--- Assets/Scripts/Destructor.cs
+++ Assets/Scripts/Destructor.cs
@@ -5,15 +5,17 @@ public class Destructor : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D toDestroy){
 
-		if(this.gameObject.tag == "EndDestructor" && toDestroy.gameObject.tag == "Points"){
-			Destroy (toDestroy.gameObject);
-		}
-
 		if(toDestroy.gameObject.tag != "Button"){
 
+			if(this.gameObject.tag == "EndDestructor" && toDestroy.gameObject.tag == "Points"){
+				Destroy (toDestroy.gameObject);
+			}
+
 			if (this.gameObject.tag == "KillerEraser") {
-				if (toDestroy.tag == "Killer") Destroy (toDestroy.gameObject);
-			}else{ 
+				if (toDestroy.tag == "Killer" || toDestroy.tag == "pointsarray") Destroy (toDestroy.gameObject);
+			}
+
+			else{ 
 				Destroy (toDestroy.gameObject);
 			}
 		}
